cb808b64f3b084f10a6b3caa2202d0e9a8554ab2
Krzysztof Cieśla <krzysztof.ciesla.1993@gmail.com>
2015-06-20 21:14:16 +0200

    okręgi bez okręgów

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,11 @@
 #include <allegro5/allegro.h>
 #include <allegro5/allegro_primitives.h>
 #include <allegro5/allegro_image.h>
+#include <vector>
+#include <list>
 #include "fun.h"
 #include "rnd.h"
+#include "Miniball.h"
 
 using namespace std;
 
@@ -22,7 +25,6 @@ int main(int argc, char const *argv[])
         al_set_new_display_flags(ALLEGRO_RESIZABLE);
         ekran=al_create_display(600,600);
         al_set_window_title(ekran,"Test");
-        ALLEGRO_KEYBOARD_STATE klawiatura;
 
         if(!ekran)
         {
@@ -71,10 +73,10 @@ int main(int argc, char const *argv[])
         bool czy=false;
         double drg=0, drg2=0, srdrg, srdrg2;
         double pol=0, pol2=0, srpol, srpol2;
-        int t=0, czas=2000;
-        int i=0, tb=1;
+        int t=0, stop=100;
+        int i=0, czas_uw=1;
         
-        while(i<N && t<=czas)
+        while(i<N && t<=stop)
         {   
             ALLEGRO_EVENT ev;
             al_wait_for_event(event_queue,&ev);
@@ -98,7 +100,7 @@ int main(int argc, char const *argv[])
             {
                 redraw=false;
 
-                while(i<N && t<=czas)
+                while(i<N && t<=stop)
                 {
                     al_clear_to_color(al_map_rgb(0,0,0));
                     czy=0;
@@ -119,7 +121,7 @@ int main(int argc, char const *argv[])
                         if (wektoryxxxx(wxN[i],wxN[j]))
                             {
                                 czy=true;
-                                tb++;
+                                czas_uw++;
                                 break;
                             }
                     }
@@ -136,8 +138,8 @@ int main(int argc, char const *argv[])
                         drg2+=(wxN[i].x)*(wxN[i].x)+(wxN[i].y)*(wxN[i].y);
                         srdrg2=drg2/i;
                         
-                        hist << tb << endl;
-                        tb=1;
+                        hist << czas_uw << endl;
+                        czas_uw=1;
                         i++;
                     }
                     /// reset
@@ -156,98 +158,50 @@ int main(int argc, char const *argv[])
                 al_flip_display();
             }
         }
-        hist << tb << endl;
+
+        std::list<std::vector<double> > punkt;
+        std::vector<double> q(2);
+        q[0] = wxN[0].poczatek.x;
+        q[1] = wxN[0].poczatek.y;
+        punkt.push_back(q);
+        for (int n = 0; n < i; n++){
+            std::vector<double> p(2);
+            p[0] = wxN[n].koniec.x;
+            p[1] = wxN[n].koniec.y;
+            punkt.push_back(p);
+        }
+
+        // szukanie najmniejszego okręgu
+        typedef std::list<std::vector<double> >::const_iterator PointIterator; 
+        typedef std::vector<double>::const_iterator CoordIterator;
+        typedef Miniball::Miniball <Miniball::CoordAccessor<PointIterator, CoordIterator> > MB;
+        MB mb(2, punkt.begin(), punkt.end());
+
+        float srodek[2];
+        float promien = sqrt(mb.squared_radius());
+        const double* center = mb.center();
+        for (int n = 0; n < 2; ++n, ++center) srodek[n] = *center;
+
+        cout << 300+10*srodek[0] << "\t" << 300+10*srodek[1] << "\t" << 10*promien << endl;
+
+        al_clear_to_color(al_map_rgb(0,0,0));
+        al_draw_circle(300+10*srodek[0], 300+10*srodek[1], 10*promien, al_map_rgb(1, 1, 0), 1);
+        for (int k = 0; k < i; k++){
+            al_draw_line(300+10*wxN[k].poczatek.x, 300+10*wxN[k].poczatek.y, 300+10*wxN[k].koniec.x, 300+10*wxN[k].koniec.y, al_map_rgb(0,255,0), 1);
+        }
+        al_flip_display();
+
+        hist << czas_uw << endl;
         plik.close();
         hist.close();
+
+        sleep(5);
+
         al_destroy_display(ekran);
     }
 
     if(!tryb)
-    {
-    fstream plik("kreski.txt",fstream::out);
-    ofstream hist("hist.txt",ios::app);
-    // srand((unsigned)time(NULL));
+    {}
 
-    rnd rnd;
-
-    int N = 200;
-    wektor wxN[N];
-
-    double a,b;
-    double dl=20.;
-    bool czy=false;
-    double drg=0, drg2=0, srdrg, srdrg2;
-    double pol=0, pol2=0, srpol, srpol2;
-    double Xp, Yp, Xk, Yk;
-    int t=0, czas=2000;
-    int i=0, tb=1;
-    
-    while(i<N && t<=czas)
-    {   
-        wxN[i].poczatek.x=0;
-        wxN[i].poczatek.y=0;
-
-        wxN[i].koniec.x=wxN[i].poczatek.x+rnd.gauss(0,dl);
-        wxN[i].koniec.y=wxN[i].poczatek.y+rnd.gauss(0,dl);
-
-        i = 1;
-
-        {
-            while(i<N && t<=czas)
-            {
-                czy=0;
-                wxN[i].poczatek.x=wxN[i-1].koniec.x;
-                wxN[i].poczatek.y=wxN[i-1].koniec.y;
-
-                a=rnd.gauss(0,dl);
-                b=rnd.gauss(0,dl);
-
-                wxN[i].koniec.x=wxN[i].poczatek.x+a;
-                wxN[i].koniec.y=wxN[i].poczatek.y+b;
-
-                for (int j = 0; j < i-1; j++)
-                {
-                    if (wektoryxxxx(wxN[i],wxN[j]))
-                        {
-                            czy=true;
-                            tb++;
-                            break;
-                        }
-                }
-
-                if(!czy)
-                {   
-                    Xp=wxN[i].poczatek.x;
-                    Yp=wxN[i].poczatek.y;
-                    Xk=wxN[i].koniec.x;
-                    Yk=wxN[i].koniec.y;
-
-                    pol+=sqrt(Xk*Xk+Yk*Yk);
-                    srpol=pol/i;
-                    pol2+=Xk*Xk+Yk*Yk;
-                    srpol2=pol2/i;
-
-                    drg+=sqrt((Xk-Xp)*(Xk-Xp)+(Yk-Yp)*(Yk-Yp));
-                    srdrg=drg/i;
-                    drg2+=(Xk-Xp)*(Xk-Xp)+(Yk-Yp)*(Yk-Yp);
-                    srdrg2=drg2/i;
-                    
-                    hist << tb << endl;
-                    tb=1;
-                    i++;
-                }
-                /// reset
-                t++;
-                czy = 0;
-
-                plik << t << "\t" << srpol << "\t" << srdrg << "\t" << srpol2 << "\t" << srdrg2 << endl;
-            }
-        }
-    }
-    hist << tb << endl;
-    plik.close();
-    hist.close();
-
-    }
     return 0;
 }
